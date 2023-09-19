611b9ae3d46c9465bfbb1a67c99bb2743803441d
Pablo <pablo@Pablos-MacBook-Air.local>
2023-09-19 16:19:14 +0300

    Done for now

--- includes/task_handler.inc.php
+++ includes/task_handler.inc.php
@@ -7,21 +7,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     require_once "./tasks_contr.inc.php";
     require_once "./tasks_model.inc.php";
     // ERROR check
-    $errors = [];
-    if (is_task_empty($task_name)) {
-        $errors["task_is_empty"] = "There is no name for task $task_name";
-    }
-    if ($errors) {
-        foreach ($errors as $error) {
-            echo "<div class=\"error-container\">$error </div>";
+    try {
+        $errors = [];
+        if (is_task_empty($task_name)) {
+            $errors["task_is_empty"] = "There is no name for task $task_name";
         }
-        unset($errors);
-        die();
+        if ($errors) {
+            $_SESSION["error_list"] = $errors;
+            header("Location: ../index.php");
+            die();
+        }
+    } catch (PDOException $e) {
+        die("Query failed: {$e->getMessage()}");
     }
     // DB update
     add_task($pdo, $task_name);
     header("Location: ../index.php");
-    die();
 } else {
     header("Location: ../index.php");
     die();
--- index.php
+++ index.php
@@ -1,32 +1,3 @@
 <?php
-require_once "./includes/config_sessions.inc.php";
+
 require_once "./includes/tasks_view.inc.php";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do List</title>
-    <link rel="stylesheet" href="./styles/main.css">
-</head>
-
-<body>
-    <div class="container">
-        <h1>To-Do List</h1>
-        <form class="input-container" action="./includes/task_handler.inc.php" method="post">
-            <input type="text" name="task_name" placeholder="Add a new task">
-            <button id="addTask">Add</button>
-        </form>
-
-        <ul class="task-list">
-            <?php
-            render_tasks();
-            ?>
-        </ul>
-    </div>
-</body>
-
-</html>
